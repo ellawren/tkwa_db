99ceabd75f2df9a520d29f4b94f8962e06a4d743
Erin Lawrence <el2891@gmail.com>
2012-02-16 09:26:38 -0600

    Done with static pages and testing setup

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,32 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "TKWA Database | Home")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          "TKWA Database | About")
     end
   end
 
